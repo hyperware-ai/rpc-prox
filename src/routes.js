c3c094a861ef97ba50acdd7f0f980c557b2fa57c
~dolled-possum <johnathanreale@icloud.com>
2025-01-03 15:40:37 -0500

    default GET

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const routes = Router();
 
-routes.get('/', (req, res) => {res.json({ message: 'invalid route' })});
+routes.get('/', (req, res) => {res.status(200).json({ message: 'default route' })});
 
 module.exports = routes;
