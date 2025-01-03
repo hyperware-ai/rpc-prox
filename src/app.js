06dbaeb2e8bbbdf93f374f172745d4509bfc4689
~dolled-possum <johnathanreale@icloud.com>
2025-01-03 16:27:08 -0500

    using Router again

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const routes = require('./routes.js');
+const routes = require('./routes.js');
 
-app.get('/default', (req, res) => {
-    res.send('Hello from Express! This is also a WebSocket proxy.');
-  });
-
-// app.use(routes);
+app.use(routes);
 
 module.exports = app;
