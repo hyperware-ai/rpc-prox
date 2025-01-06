f09bc9982088c05eae79246a6186e80c55905172
Liam Reale <liamreale@icloud.com>
2025-01-06 10:52:46 -0500

    retrying node-cache
    This reverts commit 854c60074a2989361aeee73c70d93b68090f15af.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
 const app = express();
 const routes = require('./routes.js');
 
+cache.set('restrictedProxy', false);
 app.use(routes);
 app.set('trust proxy', true);
 
-module.exports = app;
+module.exports = { app, cache };
