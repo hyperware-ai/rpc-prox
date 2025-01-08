611bad620dd42ae9dc90c12af9e4a9f1a221af7a
Liam Reale <liamreale@icloud.com>
2025-01-08 11:53:30 -0500

    added /monitor to setRestrictedProxy route

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ const localonly = require('./middlewares/localonly');
 // Routes
 routes.get('/default', (req, res) => {return res.status(200).json({ message: 'Router route' })});
 
-routes.post('/set-restricted-proxy', localonly, proxyManagementController.setRestrictedProxy);
+routes.post('/monitor/set-restricted-proxy', localonly, proxyManagementController.setRestrictedProxy);
 routes.get('/restricted-proxy-status', proxyManagementController.getRestrictedProxyStatus);
 routes.get('/user-connections', proxyManagementController.getUserConnectionsStatus);
 routes.get('/clear-user-connections', proxyManagementController.getUserConnectionsStatus);
