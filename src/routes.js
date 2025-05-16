be5a6157e15c761941b95ee17909c9e4aa3c2349
~dolled-possum <johnathanreale@icloud.com>
2025-05-16 11:36:45 -0400

    new local route for restricted proxy status

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ routes.get('/default', (req, res) => {return res.status(200).json({ message: 'Ro
 
 routes.post('/monitor/set-restricted-proxy', localonly, proxyManagementController.setRestrictedProxy);
 routes.get('/restricted-proxy-status', middlewareSecret, proxyManagementController.getRestrictedProxyStatus);
+routes.get('/monitor/restricted-proxy-status', localonly, proxyManagementController.getRestrictedProxyStatus);
 routes.get('/user-connections/:shortcode', middlewareSecret, proxyManagementController.getUserConnectionsStatus);
 routes.put('/user-connections-with-reset/:shortcode', middlewareSecret, proxyManagementController.getUserConnectionsStatus);
 
